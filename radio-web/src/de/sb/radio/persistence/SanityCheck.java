af911666bcc7c36ba64ac55094d703b8f1f04cdb
Antonia Rohne <antonia.rohne@fluffyfairygames.com>
2018-11-02 17:03:52 +0100

    adjusted to new project file system

--- radio-web/src/de/sb/radio/persistence/SanityCheck.java
+++ radio-web/src/de/sb/radio/persistence/SanityCheck.java
@@ -3,12 +3,13 @@ package de.sb.radio.persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import de.sb.radio.persistence.BaseEntity;
+
 
 public class SanityCheck {
-	static public void main(final String[]args) {
+	static public void main (final String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("radio");
 		EntityManager em = emf.createEntityManager();
-		Person p = em.find(Person.class,2L);
-		System.out.println(p.getEmail());
+		em.find(BaseEntity.class, 1L);
 	}
 }
