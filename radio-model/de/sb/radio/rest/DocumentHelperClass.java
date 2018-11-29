46dc6d41f9a09c5e89cd1833208fec798beeabcc
Vlad <vladonius@gmail.com>
2018-11-29 14:10:50 +0100

    start addDocument

--- radio-model/de/sb/radio/rest/DocumentHelperClass.java
+++ radio-model/de/sb/radio/rest/DocumentHelperClass.java
@@ -13,6 +13,8 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Response.Status;
 
 import de.sb.radio.persistence.Document;
+import de.sb.radio.persistence.Person;
+import de.sb.radio.persistence.Track;
 import de.sb.toolbox.net.RestJpaLifecycleProvider;
 
 public class DocumentHelperClass {
@@ -21,17 +23,44 @@ public class DocumentHelperClass {
 	
 	static public void main (final String[] args) throws IOException {
 		Path filePath = Paths.get(args[0]);
-		final long recordingReference = addDocument(filePath);
+		//final long recordingReference = addDocument(filePath,"music/mp3");
+		
 		//erstellen eines tracks übergebe die recordingReference
+		//createTrack(recordingReference,1);
+		addPicture(filePath);
+	
 	}
 	
-	static public long addDocument(final Path filePath) throws IOException {
+	private static void addPicture(Path filePath) throws IOException {
+		// TODO Auto-generated method stub
+		final long coverReference = addDocument(filePath,"picture/jpg");
+		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
+		final Document cover = radioManager.find(Document.class, coverReference);
+		
+	}
+
+	private static void createTrack(long recordingReference,int personID) {
+		// TODO Auto-generated method stub
+		
+		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
+		final Person owner = radioManager.find(Person.class, personID);
+		//final Album album = radioManager.find(Album.class, )
+		final Document recording = radioManager.find(Document.class, recordingReference);
+		//Track track = new Track(recording,owner,album);
+	}
+
+	private static void createAlbum() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static public long addDocument(final Path filePath, String contentType) throws IOException {
 		
 		// String filePath = "C:\\Users\\vlado\\Desktop\\Agile\\WebTechnology\\joy.mp3";
 		
 		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
 		byte[] content = Files.readAllBytes(filePath);
-		Document recording = new Document ("music/mp3", content);
+		Document recording = new Document (contentType, content);
 		radioManager.persist(recording);
 
 		try {
