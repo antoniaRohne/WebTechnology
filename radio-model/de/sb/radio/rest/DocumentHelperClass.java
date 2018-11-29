33d864af2cdb4356e4e89fd28b63883371bba2c9
Vlad <vladonius@gmail.com>
2018-11-29 15:20:17 +0100

    tried to extend addDocument , still doesn't work

--- radio-model/de/sb/radio/rest/DocumentHelperClass.java
+++ radio-model/de/sb/radio/rest/DocumentHelperClass.java
@@ -20,47 +20,50 @@ import de.sb.toolbox.net.RestJpaLifecycleProvider;
 public class DocumentHelperClass {
 
 	static private final EntityManagerFactory RADIO_FACTORY = Persistence.createEntityManagerFactory("radio");
-	
-	static public void main (final String[] args) throws IOException {
+
+	static public void main(final String[] args) throws IOException {
 		Path filePath = Paths.get(args[0]);
-		//final long recordingReference = addDocument(filePath,"music/mp3");
-		
-		//erstellen eines tracks übergebe die recordingReference
-		//createTrack(recordingReference,1);
-		addPicture(filePath);
-	
+		// final long recordingReference = addDocument(filePath,"music/mp3");
+
+		// erstellen eines tracks übergebe die recordingReference
+		// createTrack(recordingReference,1);
+		// addCover(filePath);
+		addDocument(filePath, "picture/jpg");
 	}
-	
-	private static void addPicture(Path filePath) throws IOException {
+
+	private static void addCover(Path filePath) throws IOException {
 		// TODO Auto-generated method stub
-		final long coverReference = addDocument(filePath,"picture/jpg");
+		final long coverReference = addDocument(filePath, "picture/jpg");
 		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
 		final Document cover = radioManager.find(Document.class, coverReference);
-		
+
 	}
 
-	private static void createTrack(long recordingReference,int personID) {
+	private static void createTrack(long recordingReference, int personID) {
 		// TODO Auto-generated method stub
-		
+
 		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
-		final Person owner = radioManager.find(Person.class, personID);
-		//final Album album = radioManager.find(Album.class, )
-		final Document recording = radioManager.find(Document.class, recordingReference);
+		//final Person owner = radioManager.find(Person.class, personID);  // how to get a person, in a right way ??? 
+		//final Album album = radioManager.find(Album.class, );	// how to get cover ?
+		//final Document recording = radioManager.find(Document.class,);
 		//Track track = new Track(recording,owner,album);
-	}
-
-	private static void createAlbum() {
-		// TODO Auto-generated method stub
+		//radioManager.persist(track);
 		
+		try {
+			radioManager.getTransaction().commit();
+			System.out.println("Commited");
+		} catch (PersistenceException e) {
+			throw new ClientErrorException(Status.CONFLICT);
+		} finally {
+			radioManager.getTransaction().begin();
+		}
 	}
 
 	static public long addDocument(final Path filePath, String contentType) throws IOException {
-		
-		// String filePath = "C:\\Users\\vlado\\Desktop\\Agile\\WebTechnology\\joy.mp3";
-		
+
 		final EntityManager radioManager = RADIO_FACTORY.createEntityManager();
 		byte[] content = Files.readAllBytes(filePath);
-		Document recording = new Document (contentType, content);
+		Document recording = new Document(contentType, content);
 		radioManager.persist(recording);
 
 		try {
@@ -73,5 +76,5 @@ public class DocumentHelperClass {
 		}
 
 		return recording.getIdentity();
-	}	
+	}
 }
