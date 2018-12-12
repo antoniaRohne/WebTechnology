2ab6f9abf0215bb3048902281802d2348c7f38aa
Antonia.Rohne <antonia.rohne@kolibrigames.com>
2018-12-12 11:04:29 +0100

    delete random jar, add music for testing

--- radio-web/src/WEB-INF/js/peer-radio-controller.js
+++ radio-web/src/WEB-INF/js/peer-radio-controller.js
@@ -4,8 +4,8 @@
 	// imports
 	const Controller = de_sb_radio.Controller;
 
-	let AUDIO_CONTEXT = window.AudioContext || window.webkitAudioContext;
-	let audioContext = new AUDIO_CONTEXT();
+	let localConnection;
+	let sendChannel;
 	
 	/**
 	 * Creates a new welcome controller that is derived from an abstract controller.
@@ -25,9 +25,73 @@
 		configurable: false,
 		writable: true,
 		value: async function () {
-		
+			const mainElement = document.querySelector("main");
+			mainElement.appendChild(document.querySelector("#peer-radio-template").content.cloneNode(true).firstElementChild);
+			mainElement.querySelector("#sendButton").addEventListener("click", event => this.send());
+			mainElement.querySelector("#receiveButton").addEventListener("click", event => this.listen());
+		}
+	});
+	
+	Object.defineProperty(PeerRadioController.prototype, "send", {
+		enumerable: false,
+		configurable: false,
+		writable: true,
+		value: async function () {
+
+			const mainElement = document.querySelector("main");
+			
+			mainElement.querySelector("#chooseModus").remove();
+			
+			let fileChooser = document.createElement('input') ;
+			fileChooser.type = 'file' ;
+			fileChooser.id = "fileChooser";
+			fileChooser.accept ="audio/mp3, audio/wav";	
+			fileChooser.multiple = true;
+			fileChooser.addEventListener("change", event => this.startTracks());
+			mainElement.appendChild(fileChooser);
+			
+			
+			localConnection = new RTCPeerConnection();
+
+			sendChannel = localConnection.createDataChannel("sendChannel");
+			//sendChannel.onopen = handleSendChannelStatusChange;
+			//sendChannel.onclose = handleSendChannelStatusChange;
+		}
+	});
+	
+	Object.defineProperty(PeerRadioController.prototype, "listen", {
+		enumerable: false,
+		configurable: false,
+		writable: true,
+		value: async function () {
+			
+		}
 	});
 
+	Object.defineProperty(PeerRadioController.prototype, "startTracks", {
+		enumerable: false,
+		configurable: false,
+		writable: true,
+		value: async function () {
+			const mainElement = document.querySelector("main");
+			var files = mainElement.querySelector("#fileChooser").files;
+			let fileList = document.createElement('ul');
+			
+			for(let i=0; i<files.length;i++){
+				let fileEntry = document.createElement('li');
+				fileEntry.innerHTML = files[i].name;
+				fileList.appendChild(fileEntry);
+			}	
+			
+			mainElement.appendChild(fileList);
+			
+			let audioPlayer = document.createElement("audio");
+			audioPlayer.source = URL.createObjectURL(files[0]);
+			mainElement.appendChild(audioPlayer);
+			audioPlayer.play();
+		}
+	});
+	
 	/**
 	 * Perform controller callback registration during DOM load event handling.
 	 */
