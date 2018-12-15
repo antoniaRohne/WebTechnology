cfa57178c1ee82be68ea3c1c9b701bca2c33ec4e
Vlad <vladonius@gmail.com>
2018-12-15 16:25:46 +0100

    delete "f"

--- radio-model/src/de/sb/radio/rest/EntityService.java
+++ radio-model/src/de/sb/radio/rest/EntityService.java
@@ -133,7 +133,7 @@ public class EntityService {
 	 * order to set both using the document's content and content-type.
 	 **/
 	@GET
-	@Path("f/{id}")
+	@Path("/{id}")
 	@Produces(MediaType.WILDCARD)
 	public Response queryDocument(
 		@PathParam("id") @Positive final long documentIdentity,
